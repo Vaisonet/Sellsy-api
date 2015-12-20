31a29c02b4f56a29bc08e4bacf5701932a5bd3f3
Julien Ferrier <ferjul17@gmail.com>
2015-12-20 22:31:56 +0100

    improve Request test

--- tests/Request/RequestTest.php
+++ tests/Request/RequestTest.php
@@ -5,19 +5,13 @@ namespace SellsyApi\Test\Request;
 
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Promise\Promise;
 use GuzzleHttp\Promise\PromiseInterface;
 use GuzzleHttp\Psr7\Response;
 use SellsyApi\Request\Request;
 
 class RequestTest extends \PHPUnit_Framework_TestCase {
-
-    public function testEndPoint () {
-        $request = $this->createRequest();
-        $this->assertNull($request->getEndPoint());
-        $this->assertSame($request, $request->setEndPoint('endPoint'));
-        $this->assertEquals('endPoint', $request->getEndPoint());
-    }
 
     /**
      * @return Request
@@ -26,37 +20,30 @@ class RequestTest extends \PHPUnit_Framework_TestCase {
         return new Request('userToken', 'userSecret', 'consumerToken', 'consumerSecret');
     }
 
-    public function testCall () {
-        $request = $this->createRequest();
-        $prop    = new \ReflectionProperty($request, 'client');
+    /**
+     * @param Request $request
+     * @param string  $methodToCall
+     * @param string  $method
+     * @param mixed   $params
+     * @param mixed   $return
+     */
+    protected function prepareRequest (Request $request, $methodToCall, $method, $params, $return) {
+        $prop = new \ReflectionProperty($request, 'client');
         $prop->setAccessible(TRUE);
-        $params = ['field' => 'value'];
-        $method = 'method';
-        $client = $this->createClientMock('post', $method, $params, '{"status":"success","response":"result"}');
+        $client = $this->createClientMock($methodToCall, $method, $params, $return);
         $prop->setValue($request, $client);
-        $this->assertEquals('result', $request->call($method, $params));
-    }
-
-    public function testCallAsync () {
-        $request = $this->createRequest();
-        $prop    = new \ReflectionProperty($request, 'client');
-        $prop->setAccessible(TRUE);
-        $params  = ['field' => 'value'];
-        $method  = 'method';
-        $promise = new Promise();
-        $client  = $this->createClientMock('postAsync', $method, $params, $promise);
-        $prop->setValue($request, $client);
-        $res = $request->callAsync($method, $params);
-        $this->assertInstanceOf(PromiseInterface::class, $res);
-        $promise->resolve(new Response(200, [], '{"status":"success","response":"result"}'));
-        $this->assertEquals('result', $res->wait());
     }
 
     /**
+     * @param string $methodToMock
+     * @param string $methodName
+     * @param mixed  $params
+     * @param mixed  $result
+     *
      * @return \PHPUnit_Framework_MockObject_MockObject
      */
     protected function createClientMock ($methodToMock, $methodName, $params, $result) {
-        $client = $this->getMock(Client::class, [$methodToMock]);
+        $client = $this->getMock(Client::class, [$methodToMock], [['base_uri' => 'http://api.sellsy.com']]);
         $client->expects($this->once())->method($methodToMock)->willReturnCallback(function ($method, $options) use (
             $methodName, $params, $result
         ) {
@@ -81,6 +68,59 @@ class RequestTest extends \PHPUnit_Framework_TestCase {
             return $result;
         });
         return $client;
+    }
+
+    public function testEndPoint () {
+        $request = $this->createRequest();
+        $this->assertNull($request->getEndPoint());
+        $this->assertSame($request, $request->setEndPoint('endPoint'));
+        $this->assertEquals('endPoint', $request->getEndPoint());
+    }
+
+    public function testCall () {
+        $request = $this->createRequest();
+        $prop    = new \ReflectionProperty($request, 'client');
+        $prop->setAccessible(TRUE);
+        $params = ['field' => 'value'];
+        $method = 'method';
+        $client = $this->createClientMock('post', $method, $params, '{"status":"success","response":"result"}');
+        $prop->setValue($request, $client);
+        $this->assertEquals('result', $request->call($method, $params));
+    }
+
+    public function testCallAsync () {
+        $params  = ['field' => 'value'];
+        $method  = 'method';
+        $promise = new Promise();
+        $request = $this->createRequest();
+        $this->prepareRequest($request, 'postAsync', $method, $params, $promise);
+        $res = $request->callAsync($method, $params);
+        $this->assertInstanceOf(PromiseInterface::class, $res);
+        $promise->resolve(new Response(200, [], '{"status":"success","response":"result"}'));
+        $this->assertEquals('result', $res->wait());
+    }
+
+    public function testCallAsyncWithRequestError () {
+        $promise = new Promise();
+        $request = $this->createRequest();
+        $this->prepareRequest($request, 'postAsync', '', '', $promise);
+        $res = $request->callAsync('', '');
+        $this->assertInstanceOf(PromiseInterface::class, $res);
+        $promise->reject($this->getMockBuilder(RequestException::class)->disableOriginalConstructor()->getMock());
+        $otherwiseCount = 0;
+        $res->then(function () {
+            throw new \PHPUnit_Framework_ExpectationFailedException('Fullfilled callback was not expected to be called.');
+        }, function ($e) use (&$otherwiseCount) {
+            ++$otherwiseCount;
+            return $e;
+        });
+        try {
+            $res->wait();
+        } catch (RequestException $e) {
+        }
+        if ($otherwiseCount != 1) {
+            throw new \PHPUnit_Framework_ExpectationFailedException('OnRejected callback was expected to be called once.');
+        }
     }
 
 }
