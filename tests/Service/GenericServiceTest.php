60a5c5644b3d74c4b1b718ffbc2decd040f04c8f
Julien Ferrier <ferjul17@gmail.com>
2016-01-18 10:01:50 +0100

    add test of retryable methods

--- tests/Service/GenericServiceTest.php
+++ tests/Service/GenericServiceTest.php
@@ -70,4 +70,80 @@ class GenericServiceTest extends \PHPUnit_Framework_TestCase {
 
     }
 
+    public function testRetryableCall () {
+        /**
+         * @var RequestInterface $mockRequest
+         */
+        $mockRequest = $this->getMock(RequestInterface::class);
+        $mockRequest->expects($this->once())->method('call')
+                    ->with($this->equalTo('name.method'), $this->equalTo(['field' => 'value']))->willReturn('result');
+        $service = new GenericService($mockRequest, 'name');
+        $res     = $service->retryableCall(function () use ($service) {
+            $args = func_get_args();
+            $this->assertCount(3, $args);
+            $this->assertSame($service, $args[0]);
+            $this->assertSame(0, $args[1]);
+            $this->assertNull($args[2]);
+            return ['method', ['field' => 'value']];
+        });
+        $this->assertEquals('result', $res);
+
+    }
+
+    public function testRetryableCallAsync () {
+        /**
+         * @var AsyncRequestInterface $mockRequest
+         */
+        $mockPromise = $this->getMock(PromiseInterface::class);
+        $mockRequest = $this->getMock(AsyncRequestInterface::class);
+        $mockRequest->expects($this->once())->method('callAsync')
+                    ->with($this->equalTo('name.method'), $this->equalTo(['field' => 'value']))
+                    ->willReturn($mockPromise);
+        $service = new GenericService($mockRequest, 'name');
+        $res     = $service->retryableCallAsync(function () use ($service) {
+            $args = func_get_args();
+            $this->assertCount(3, $args);
+            $this->assertSame($service, $args[0]);
+            $this->assertSame(0, $args[1]);
+            $this->assertNull($args[2]);
+            return ['method', ['field' => 'value']];
+        });
+        $this->assertSame($mockPromise, $res);
+
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Exception thrown by the function of the user
+     */
+    public function testErrorOnRetryableCall () {
+        /**
+         * @var RequestInterface $mockRequest
+         */
+        $mockRequest = $this->getMock(RequestInterface::class);
+        $e           = new \Exception;
+        $mockRequest->expects($this->exactly(3))->method('call')
+                    ->with($this->equalTo('name.method'), $this->equalTo(['field' => 'value']))
+                    ->will($this->throwException($e));
+        $service = new GenericService($mockRequest, 'name');
+        $retryNb  = 0;
+        $service->retryableCall(function () use ($service, &$retryNb, $e) {
+            $args = func_get_args();
+            $this->assertCount(3, $args);
+            $this->assertSame($service, $args[0]);
+            $this->assertSame($retryNb, $args[1]);
+            if ($retryNb) {
+                $this->assertSame($e, $args[2]);
+                if ($retryNb >= 3) {
+                    throw new \Exception('Exception thrown by the function of the user');
+                }
+            } else {
+                $this->assertNull($args[2]);
+            }
+            $retryNb++;
+            return ['method', ['field' => 'value']];
+        });
+
+    }
+
 }
